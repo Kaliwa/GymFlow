c0cd2d650920f84bd38d119ff440f05b42622928
Pierre GUEIT-DESSUS <pierre@digipharmacie.fr>
2025-07-18 09:45:46 +0200

    Construct tree and init express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello, World !");
+import { config } from "dotenv";
+import express  from "express";
+config();
+
+async function startServer() {
+    await bootstrapAPI();
+    const app = express();
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}
+
+async function bootstrapAPI() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("API has been bootstrapped.");
+            resolve(true);
+        }, 1000);
+    });
+}
+
+startServer().then(() => {
+    console.log("API bootstrapped successfully.");
+}).catch((error) => {
+    console.error("Error bootstrapping API:", error);
+    process.exit(1);
+});
